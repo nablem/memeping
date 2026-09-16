08798188da0a8ab6eb72a7ae07ac440537453216
Nabil Lemenuel <nabil.lemenuel@gmail.com>
2026-09-17 00:58:30 +0200

    add rustler config

--- config/prod.exs
+++ config/prod.exs
@@ -30,3 +30,5 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
+
+config :rustler_precompiled, :force_build, ex_secp256k1: true
