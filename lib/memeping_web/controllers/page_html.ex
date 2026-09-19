b5c21afc3bfef726f32b70fab66e0ae537d104b6
Nabil Lemenuel <nabil.lemenuel@gmail.com>
2026-09-19 19:29:13 +0200

    add favicon, fix schema markup

--- lib/memeping_web/controllers/page_html.ex
+++ lib/memeping_web/controllers/page_html.ex
@@ -54,19 +54,24 @@ defmodule MemePingWeb.PageHTML do
   end
 
   defp plan_offer_json_ld(plan) do
-    %{
+    offer = %{
       "@type" => "Offer",
       "name" => plan.name,
       "price" => plan.price_cents / 100,
-      "priceCurrency" => "USD",
-      "priceSpecification" => %{
+      "priceCurrency" => "USD"
+    }
+
+    if plan.duration_days do
+      Map.put(offer, "priceSpecification", %{
         "@type" => "UnitPriceSpecification",
         "price" => plan.price_cents / 100,
         "priceCurrency" => "USD",
-        "billingDuration" => plan.duration_days || 30,
+        "billingDuration" => "P#{plan.duration_days}D",
         "unitCode" => "DAY"
-      }
-    }
+      })
+    else
+      offer
+    end
   end
 
   defp absolute_url(path), do: MemePingWeb.Endpoint.url() <> path
--- test/memeping_web/controllers/page_controller_test.exs
+++ test/memeping_web/controllers/page_controller_test.exs
@@ -3,6 +3,18 @@ defmodule MemePingWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Catch every memecoin call"
+    html = html_response(conn, 200)
+
+    json_ld_blocks =
+      Regex.scan(~r/<script type="application\/ld\+json">\s*(.*?)\s*<\/script>/s, html,
+        capture: :all_but_first
+      )
+
+    json_ld = Enum.map(json_ld_blocks, &Jason.decode!(hd(&1)))
+
+    assert html =~ "Catch every memecoin call"
+    assert length(json_ld) == 2
+    assert Enum.any?(json_ld, &String.contains?(Jason.encode!(&1), "P30D"))
+    refute html =~ "{raw(Jason.encode!(data))}"
   end
 end
