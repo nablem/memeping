31e9746db6737eeeef47cf5ae95c7c0c4f5b5476
Nabil Lemenuel <nabil.lemenuel@gmail.com>
2026-09-19 19:39:05 +0200

    fix favicon

--- test/memeping_web/controllers/page_controller_test.exs
+++ test/memeping_web/controllers/page_controller_test.exs
@@ -13,6 +13,7 @@ defmodule MemePingWeb.PageControllerTest do
     json_ld = Enum.map(json_ld_blocks, &Jason.decode!(hd(&1)))
 
     assert html =~ "Catch every memecoin call"
+    assert html =~ ~s(<link rel="icon" href="/favicon.ico" type="image/x-icon">)
     assert length(json_ld) == 2
     assert Enum.any?(json_ld, &String.contains?(Jason.encode!(&1), "P30D"))
     refute html =~ "{raw(Jason.encode!(data))}"
